ef7827bc12d539d92c5c5e8dafee83fdb8b6e0ed
ssulphurr <ssulphurr@gmail.com>
2023-02-24 20:34:51 +0200

    adds markup to 2st tast

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// markup
+
+const galleryItemsMarkup = galleryItems
+.map(({preview, original, description}) => 
+    `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `)
+    .join("");
+
+const galleryEl = document.querySelector(".gallery")
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryItemsMarkup)
